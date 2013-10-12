3874b949a18812f241670bbd97a26dacb4bce9b6
jeffreybiles <bilesjeffrey@gmail.com>
2013-10-12 01:52:41 -0500

    initial commit, with task already created on rails end

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DailyChecklist::Application.config.secret_key_base = 'a026b1bed5e541c5134881e5575e3947254aa48755b89f7b1aa9f4bf521fa34aed4d3eb80d27cb67d75e7677e8fb2b09c226cfe23f71a3f4dd4213952c694610'
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Task.create([
+              { title: 'floss', details: 'do it at night' },
+              { title: 'shower', details: 'do it in the morning'}
+            ])
